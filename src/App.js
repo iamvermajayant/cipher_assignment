ffe58246e660f0fa01ecf558e7de494fee05786c
Jayant Verma <iamvermajayant@gmail.com>
2022-06-26 18:59:33 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import "./App.css";
 import Product from "./Components/Product";
 
+const productItem = [
+  {
+    productTitle: "Iphone",
+    productImage: "https://m.media-amazon.com/images/I/61-r9zOKBCL._SX522_.jpg",
+    price: "1.2Lakh",
+  },
+  {
+    productTitle: "laptop",
+    productImage: "https://m.media-amazon.com/images/I/61-r9zOKBCL._SX522_.jpg",
+    price: "1.2Lakh",
+  },
+  {
+    productTitle: "samsung ",
+    productImage: "https://m.media-amazon.com/images/I/61-r9zOKBCL._SX522_.jpg",
+    price: "1.2Lakh",
+  },
+];
+
 function App() {
   return (
     <div className="App">
-      {Product.map((products) => (
-        <div className="product-container">
-          <img src={products.productImage} alt={products.productTitle} />
-          <h2>{products.productTitle}</h2>
-          <div className="product-price">
-            <h4>Price : {products.price}</h4>
-          </div>
-          <div className="product-buttons-wrapper">
-            <button>Add to cart</button>
-            <button>Make offer</button>
-          </div>
-        </div>
-      ))}
+      <Product productItem = {productItem}/>
     </div>
   );
 }
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,7 +1,23 @@
-const Product = [
-  { "productTitle": "Iphone", "productImage" : "https://m.media-amazon.com/images/I/61-r9zOKBCL._SX522_.jpg" , "price" : "1.2Lakh"},
-  { "productTitle": "laptop", "productImage" : "https://m.media-amazon.com/images/I/61-r9zOKBCL._SX522_.jpg" , "price" : "1.2Lakh"},
-  { "productTitle": "samsung ", "productImage" : "https://m.media-amazon.com/images/I/61-r9zOKBCL._SX522_.jpg", "price" : "1.2Lakh" },
-];
+import React from "react";
+
+const Product = ({productItem}) => {
+  return (
+    <div>
+      {productItem.map((products) => (
+        <div className="product-container">
+          <img src={products.productImage} alt={products.productTitle} />
+          <h2>{products.productTitle}</h2>
+          <div className="product-price">
+            <h4>Price : {products.price}</h4>
+          </div>
+          <div className="product-buttons-wrapper">
+            <button>Add to cart</button>
+            <button>Make offer</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Product;
